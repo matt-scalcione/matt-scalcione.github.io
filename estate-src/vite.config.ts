7977e8563450e6aa34c92f37be42c2ab07e527b2
matt-scalcione <131294010+matt-scalcione@users.noreply.github.com>
2025-11-14 12:32:06 -0500

    Add legacy Vite plugin for Safari support

--- estate-src/vite.config.ts
+++ estate-src/vite.config.ts
@@ -1,14 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import legacy from '@vitejs/plugin-legacy';
 
 export default defineConfig({
   base: '/estate/',
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ['defaults', 'not IE 11', 'iOS >= 12', 'Safari >= 12'],
+      modernPolyfills: true,
+      renderLegacyChunks: true,
+    }),
+  ],
   build: {
     outDir: '../estate',
     assetsDir: 'assets',
-    target: ['es2018', 'safari14'],
+    target: ['es2015'],
     cssTarget: 'safari14',
+    minify: 'terser',
+    terserOptions: { safari10: true, ecma: 2015 },
     sourcemap: false,
   },
 });
