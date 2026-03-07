9a697d1b7e948472b2e778bb91d04aa489039407
Matt Scalcione <matt@users.noreply.github.com>
2026-03-06 22:40:49 -0500

    Restore match escapeHtml helper

--- match.js
+++ match.js
@@ -1046,6 +1046,15 @@ function escapeRegex(value) {
   return String(value || "").replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
 }
 
+function escapeHtml(value) {
+  return String(value ?? "")
+    .replaceAll("&", "&amp;")
+    .replaceAll("<", "&lt;")
+    .replaceAll(">", "&gt;")
+    .replaceAll("\"", "&quot;")
+    .replaceAll("'", "&#39;");
+}
+
 function displayPlayerHandle(name, teamName) {
   const raw = String(name || "").trim();
   if (!raw) {
